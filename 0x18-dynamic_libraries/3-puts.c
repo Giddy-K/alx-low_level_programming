5b1ea7d48218f60ecd597fab380f8dd41fc04a6c
Giddy-K <gideonkipamet@gmail.com>
2024-01-29 13:21:46 -0800

    Finished task 0

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _puts -prints a string to the console
+ * @s: pointer to the string to be printed
+ */
+void _puts(char *s)
+{
+	puts(s);
+}
